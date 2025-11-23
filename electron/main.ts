b85078b6e4749063625b037f401251c4b8575487
Vincent Faigt <vincent.faigt@outlook.com>
2025-11-23 17:54:45 +0000

    Add a strict CSP

--- electron/main.ts
+++ electron/main.ts
@@ -3,6 +3,8 @@ import * as path from 'node:path';
 
 let mainWindow: BrowserWindow | null = null;
 
+const isDev = Boolean(process.env.VITE_DEV_SERVER_URL);
+
 async function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1200,
@@ -10,11 +12,26 @@ async function createWindow() {
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
+      sandbox: true,
+      nodeIntegration: false,
     },
   });
 
-  if (process.env.VITE_DEV_SERVER_URL) {
-    await mainWindow.loadURL(process.env.VITE_DEV_SERVER_URL);
+  if (!isDev) {
+    mainWindow.webContents.session.webRequest.onHeadersReceived((details, callback) => {
+      callback({
+        responseHeaders: {
+          ...details.responseHeaders,
+          'Content-Security-Policy': [
+            "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'; connect-src 'self';",
+          ],
+        },
+      });
+    });
+  }
+
+  if (isDev) {
+    await mainWindow.loadURL(process.env.VITE_DEV_SERVER_URL as string);
     mainWindow.webContents.openDevTools();
   } else {
     await mainWindow.loadFile(path.join(__dirname, '../renderer/index.html'));
